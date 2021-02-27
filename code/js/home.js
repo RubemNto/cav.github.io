b2edb64a954f4e1c13ef65277ef5bf90c2eba12b
rubem <rubemchrist@gmail.com>
2021-02-27 10:53:19 +0000

    .

---
+++ code/js/home.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    var backgroundImg=["/images/donuts.jpg",
+                      "/images/salads.jpg",
+                      "/images/coffee.jpg",
+                      ]
+    setInterval(changeImage, 5000);
+
+    function changeImage() {   
+        var i = Math.floor(Math.random() * Math.floor(backgroundImg.length))        
+        document.getElementById("headerImages").style.backgroundImage = "url('"+backgroundImg[i]+"')";
+        setInterval(5000);
+        //   document.body.style.backgroundImage = "url('"+backgroundImg[i]+"')";
+    }
+        
+  }
